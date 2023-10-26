a58239aa08e4c9689c9236558cb6d8d313899372
Harald Mörk Jonsson <morkjonssonharald@gmail.com>
2023-10-27 01:54:13 +0200

    SurfaceController added

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,40 @@
+﻿using Crito.Models;
+/*using Crito.Services;*/
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace Crito.Controllers;
+
+public class ContactsController : SurfaceController
+{
+    public ContactsController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+    {
+    }
+
+    [HttpPost]
+    public /*async Task<*/IActionResult/*>*/ Index(ContactForm contactForm)
+    {
+        if (!ModelState.IsValid)
+        {
+            return CurrentUmbracoPage();
+        }
+
+/*        using var mail = new MailService("no-reply@crito.com", "smtp.websupport.se", 465, "morkjonssonharald@gmail.com", "BytMig123!");
+
+        // to sender
+        await mail.SendAsync(contactForm.Email, "Your contact request was received", "Hi, we've received your request and will be in contact soon.").ConfigureAwait(false);
+
+        // to us
+        await mail.SendAsync("morkjonssonharald@gmail.com", $"{contactForm.Name} sent a request", $"{contactForm.Message}").ConfigureAwait(false);*/
+
+        return LocalRedirect(contactForm.RedirectURL ?? "/");
+
+        
+    }
+}
---
+++ Models/ContactForm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crito.Models;
+
+public class ContactForm
+{
+    [Required]
+    public string Name { get; set; } = null!;
+    
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    public string Message { get; set; } = null!;
+    public string? RedirectURL { get; set; } = "/contact";
+}
